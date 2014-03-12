ca5a74faae6f0dc67248957df25493a93a8932a7
Sylvia Wun <yfswun@yahoo.com>
2014-03-11 23:03:37 -0700

    Change test code

--- test/integration/static_pages_test.rb
+++ test/integration/static_pages_test.rb
@@ -1,34 +1,37 @@
 require 'test_helper'
 
 class StaticPagesTest < ActionDispatch::IntegrationTest
+
+  @@titleMain = 'Ruby on Rails Tutorial Sample App'
+
   test "Home page should have the content 'Sample App'" do
     get '/static_pages/home'
-    assert_response :success
-    assert_select 'body', /Sample App/
+    assert response.body.include?('Sample App')
   end
-  test "Help page should have the content 'Help'" do
-    get '/static_pages/help'
-    assert_response :success
-    assert_select 'body', /Help/
-  end
-  test "About page should have the content 'About Us'" do
-    get '/static_pages/about'
-    assert_response :success
-    assert_select 'body', /About Us/
-  end
+
   test "Home page should have the title 'Home'" do
     get '/static_pages/home'
-    assert_response :success
-    assert_select 'title', 'Ruby on Rails Tutorial Sample App | Home'
+    assert_select 'title', "#{@@titleMain} | Home"
   end
+
+  test "Help page should have the content 'Help'" do
+    get '/static_pages/help'
+    assert response.body.include?('Help')
+  end
+
   test "Help page should have the title 'Help'" do
     get '/static_pages/help'
-    assert_response :success
-    assert_select 'title', 'Ruby on Rails Tutorial Sample App | Help'
+    assert_select 'title', "#{@@titleMain} | Help"
   end
+
+  test "About page should have the content 'About Us'" do
+    get '/static_pages/about'
+    assert response.body.include?('About Us')
+  end
+
   test "About page should have the title 'About Us'" do
     get '/static_pages/about'
-    assert_response :success
-    assert_select 'title', 'Ruby on Rails Tutorial Sample App | About Us'
+    assert_select 'title', "#{@@titleMain} | About Us"
   end
+
 end
