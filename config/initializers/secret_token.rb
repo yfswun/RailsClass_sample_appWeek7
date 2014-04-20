4055befb0c9611fa7dc1e8f2a729a12f29a66037
Sylvia Wun <yfswun@yahoo.com>
2014-04-19 17:51:52 -0700

    Make a basic User model (including secure passwords)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = 'f92e61ff6f86178d598a537826da124f87a77d2f02ab82e9c4e232e452c2882b3d065221d71bd989d327f21c6914234476e363d802e8763eb7016cd6095b8846'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
