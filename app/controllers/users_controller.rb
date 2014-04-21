6a80b5d142d1ff3dfa16dfb3d2666c5b237dfde2
Sylvia Wun <yfswun@yahoo.com>
2014-04-20 22:59:36 -0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    # respond_to do |format|
+      if @user.save
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+        # format.html { redirect_to @user, notice: 'User was successfully created.' }
+        # format.json { render action: 'show', status: :created, location: @user }
+      else
+        render 'new'
+        # format.html { render action: 'new' }
+        # format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    # end
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+﻿FactoryGirl.define do
+  factory :user do
+    name     "Michael Hartl"
+    email    "michael@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
